7f36ab517c3000e9344ed71c523792eefbd3eb9f
Thiagodfarias <thiago__07@hotmail.com>
2024-06-04 16:54:43 -0400

    inclusao de testes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://www.minimundi.com.br/cdn/imagens/produtos/original/batmobile_e_batman_124_metal_die_cast_a.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
